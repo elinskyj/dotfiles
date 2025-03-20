ea58aa26d90ae9a40fc0b0512bbc5196e7c53efc
elinskyj <john.elinsky@gmail.com>
2025-03-19 20:45:33 -0400

    feat(nvim): show hidden files by default in neotree and telescope

--- nvim/.config/nvim/lua/plugins/neotree.lua
+++ nvim/.config/nvim/lua/plugins/neotree.lua
@@ -7,18 +7,14 @@ return {
     "MunifTanjim/nui.nvim",
   },
   config = function()
-    vim.keymap.set('n', '<C-n>', ':Neotree filesystem toggle left<CR>', {})
-
-    -- Function to toggle NeoTree and focus on the file if one is opened
-    local function open_neotree()
-      vim.cmd(":Neotree filesystem toggle left")
-      vim.cmd("wincmd p")
-    end
-
-    if vim.fn.argc() == 0 then
-      vim.cmd(":Neotree filesystem toggle left")
-    else
-      vim.defer_fn(open_neotree, 50)
-    end
+    require("neo-tree").setup({
+      filesystem = {
+        filtered_items = {
+          visible = true
+        }
+      }
+    })
+    vim.keymap.set('n', '<C-n>', ':Neotree float toggle <CR>', {})
+    vim.keymap.set('n', '<leader>fs', ':Neotree filesystem toggle left<CR>', {})
   end
 }
