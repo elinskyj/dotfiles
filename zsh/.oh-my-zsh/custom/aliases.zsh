58e4877f7211c7edff84e234707882b526031160
elinskyj <john.elinsky@gmail.com>
2025-03-20 08:39:02 -0400

    feat: add wttr.in v2 alias and change unit

--- zsh/.oh-my-zsh/custom/aliases.zsh
+++ zsh/.oh-my-zsh/custom/aliases.zsh
@@ -42,4 +42,5 @@ alias gcu="git reset HEAD~"
 # ---------------------------------------------------------------------------------------
 # misc
 # weather report
-alias weather="curl wttr.in"
+alias weather="curl 'wttr.in/?M'"
+alias weather2="curl 'v2.wttr.in/?M'"
