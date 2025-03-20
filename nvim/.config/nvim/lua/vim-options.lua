22002e3ad8e9329d784e2c8b2e014c4a78378d24
elinskyj <john.elinsky@gmail.com>
2025-03-19 20:51:22 -0400

    feat(nvim): update config

--- nvim/.config/nvim/lua/vim-options.lua
+++ nvim/.config/nvim/lua/vim-options.lua
@@ -2,8 +2,9 @@ vim.cmd("set expandtab")
 vim.cmd("set tabstop=2")
 vim.cmd("set softtabstop=2")
 vim.cmd("set shiftwidth=2")
-vim.cmd("set number")
+vim.cmd("set relativenumber")
 vim.g.mapleader = " "
+vim.opt.clipboard = "unnamedplus"
 
 -- Navigate vim panes better
 vim.keymap.set('n', '<c-k>', ':wincmd k<CR>')
