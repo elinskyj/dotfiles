bfe85a05c94e64a87dfadcc8a611674c8e07224b
elinskyj <john.elinsky@gmail.com>
2025-04-10 19:38:52 -0400

    feat(nvim): add lazyvim
    https://github.com/LazyVim/LazyVim/

---
+++ lazyvim/.config/nvim/lua/plugins/neotree.lua
@@ -0,0 +1,20 @@
+return {
+  "nvim-neo-tree/neo-tree.nvim",
+  branch = "v3.x",
+  dependencies = {
+    "nvim-lua/plenary.nvim",
+    "nvim-tree/nvim-web-devicons",
+    "MunifTanjim/nui.nvim",
+  },
+  config = function()
+    require("neo-tree").setup({
+      filesystem = {
+        filtered_items = {
+          visible = true,
+        },
+      },
+    })
+    vim.keymap.set("n", "<C-n>", ":Neotree float toggle <CR>", {})
+    vim.keymap.set("n", "<leader>fs", ":Neotree filesystem toggle left<CR>", {})
+  end,
+}
