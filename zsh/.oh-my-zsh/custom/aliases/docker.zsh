dea3f6f12cd29802e61dbbf757bc591ac49c3c21
elinskyj <john.elinsky@gmail.com>
2025-04-12 22:36:43 -0400

    feat(zsh): add commands from other machines

---
+++ zsh/.oh-my-zsh/custom/aliases/docker.zsh
@@ -0,0 +1,1 @@
+alias de="docker exec -it"
--- zsh/.oh-my-zsh/custom/aliases/utilities.zsh
+++ zsh/.oh-my-zsh/custom/aliases/utilities.zsh
@@ -1,8 +1,15 @@
+# theme chooser
+alias themel="~/.oh-my-zsh/tools/theme_chooser.sh -l | less"
+alias themes="~/.oh-my-zsh/tools/theme_chooser.sh -s | less"
+
 # GNU Stow
 alias unstow="stow -D"
 
 # neovim
 alias vi="nvim"
+
+# tmux
+alias t="tmux"
 
 # use batcat on debian or bat on other systems instead of cat
 if command -v batcat &> /dev/null; then
