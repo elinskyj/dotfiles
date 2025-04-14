544759b7e4101c23125967a5ca671d02b2236792
elinskyj <john.elinsky@gmail.com>
2025-04-14 16:57:59 -0400

    feat(nvim): add transparency

--- lazyvim/.config/nvim/init.lua
+++ lazyvim/.config/nvim/init.lua
@@ -1,2 +1,15 @@
 -- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
+function Transparent(color)
+  color = color or "catppuccin"
+  vim.cmd.colorscheme(color)
+  vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
+  vim.api.nvim_set_hl(0, "NormalNC", { bg = "none" })
+  vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
+  vim.api.nvim_set_hl(0, "NeoTreeNormal", { bg = "none" })
+  vim.api.nvim_set_hl(0, "NeoTreeNC", { bg = "none" })
+  vim.api.nvim_set_hl(0, "NeoTreeWinSeparator", { bg = "none" })
+  vim.api.nvim_set_hl(0, "NeoTreeEndOfBuffer", { bg = "none" })
+  vim.api.nvim_set_hl(0, "EndOfBuffer", { bg = "none" })
+end
+Transparent()
