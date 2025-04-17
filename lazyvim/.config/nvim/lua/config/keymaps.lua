abdef945f7beae16b8345b16e2e616f53267cd8b
elinskyj <john.elinsky@gmail.com>
2025-04-17 19:25:25 -0400

    feat(nvim): add keymaps for copilot

--- lazyvim/.config/nvim/lua/config/keymaps.lua
+++ lazyvim/.config/nvim/lua/config/keymaps.lua
@@ -1,3 +1,9 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+
+-- copilot completion
+vim.keymap.set("i", "<C-L>", "<plug>(copilot-accept-word)")
+vim.keymap.set("i", "<C-N>", "<plug>(copilot-next)")
+vim.keymap.set("i", "<C-P>", "<plug>(copilot-previous)")
+vim.keymap.set("i", "<C-E>", "<plug>(copilot-dismiss)")
