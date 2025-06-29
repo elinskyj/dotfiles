3ec7e083d59dbb41d7453b407a958c0756557517
elinskyj <john.elinsky@gmail.com>
2025-06-29 19:49:54 -0400

    feat(nvim): add lsp toggle keymaps

--- lazyvim/.config/nvim/lua/config/keymaps.lua
+++ lazyvim/.config/nvim/lua/config/keymaps.lua
@@ -7,3 +7,7 @@ vim.keymap.set("i", "<C-L>", "<plug>(copilot-accept-word)")
 vim.keymap.set("i", "<C-N>", "<plug>(copilot-next)")
 vim.keymap.set("i", "<C-P>", "<plug>(copilot-previous)")
 vim.keymap.set("i", "<C-E>", "<plug>(copilot-dismiss)")
+
+-- toggle lsp
+vim.keymap.set("n", "<leader>xe", ":LspStart<CR>", { desc = "Start LSP" })
+vim.keymap.set("n", "<leader>xd", ":LspStop<CR>", { desc = "Stop LSP" })
