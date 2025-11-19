e25e6016c26622fcbaadaf710aa20b9646452a22
ElinskyJ <john@elinsky.cc>
2025-11-19 12:37:40 -0500

    feat(zsh): enhance alias management and discoverability

--- zsh/.zsh/custom/shell.zsh
+++ zsh/.zsh/custom/shell.zsh
@@ -1,9 +1,17 @@
 # Editor shortcuts
 alias zshrc="${EDITOR:-vi} ~/.zshrc"
-alias aliases="${EDITOR:-vi} $ZSH_CUSTOM"
+
+# edit aliases in $ZSH_CUSTOM
+ae() {
+    ls $ZSH_CUSTOM | fzf --preview="batcat $ZSH_CUSTOM/{} --color always" --layout=reverse --cycle --border -m\
+      --prompt="Select alias to edit: " --header="Aliases in $ZSH_CUSTOM"\
+      --bind "change:unbind(one),enter:become(${EDITOR:-vi} $ZSH_CUSTOM/{+}),one:become(${EDITOR:-vi} $ZSH_CUSTOM/{+})" -q " $@"
+}
 
 # search aliases (ag [partial command or alias])
-alias ag="alias | grep"
+ag() {
+  eval $(alias | fzf --border --prompt='Select alias to run: ' -1 -q " $@" | cut -d'=' -f1)
+}
 
 # search command history (hg [partial command])
 alias hg="history | grep"
