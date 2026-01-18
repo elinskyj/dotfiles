9754aa0411c013251d2073a7fb4d22f1cd5e4140
elinskyj <john.elinsky@gmail.com>
2026-01-18 16:16:37 -0500

    feat(snapraid): use _fzfhelp_menu for command list

--- zsh/.zsh/custom/snapraid.zsh
+++ zsh/.zsh/custom/snapraid.zsh
@@ -1,16 +1,22 @@
 alias srrconf="sudoedit /root/snapraid-runner/snapraid-runner.conf"
-alias srlog="tail -n35 /var/log/snapraid.log"
 alias srconf="sudoedit /etc/snapraid.conf"
-alias srstat="sudo snapraid status"
-alias srdiff="sudo snapraid diff"
-alias srsync="sudo snapraid sync"
-alias srlist="sudo snapraid list"
-alias srdup="sudo snapraid dup"
-alias srup="sudo snapraid up"
-alias srdn="sudo snapraid down"
-alias srtch="sudo snapraid touch"
-alias srsmrt="sudo snapraid smart"
-alias srpool="sudo snapraid pool"
-alias srchk="sudo snapraid check"
-alias srfix="sudo snapraid fix -e"
-alias sr="sudo snapraid"
+alias sr="snapraid_fzf"
+
+snapraid_fzf() {
+  local extra=(
+      $'help\tShow help information'
+      $'log\tShow snapraid logs'
+    ) # append extra commands
+
+  local selected=$(_fzfhelp_menu "snapraid" "$1" "${extra[@]}")
+  if [ -n "$selected" ]; then
+    case "$selected" in
+      help)   snapraid --help ;;
+      fix)    sudo snapraid fix -e ;;
+      log)    less +G < /var/log/snapraid.log ;;
+      scrub)  print -z "sudo snapraid scrub --plan {{PLAN}} {{--older-than DAYS}}" ;;
+      diff)   less < <(sudo snapraid diff) ;;
+      *)      sudo snapraid "$selected" ;;
+    esac
+  fi
+}
